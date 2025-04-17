e1335404be3bd967374f5115882cdb31578eda55
Harsh <harshbhagat583@gmail.com>
2025-04-17 11:27:49 +0530

    add viteconfig

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 3000, // Optional: Change the port if needed
+    strictPort: true, // Optional: Fail if the port is already in use
+  },
+})
